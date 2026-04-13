0278b4ea17e1f18307857d68ad54ed6ffa00fdaf
Martin Suarez <martinsuarezjaramillo@gmail.com>
2026-04-12 19:48:10 -0500

    Finished rk4step

--- rk4step.m
+++ rk4step.m
@@ -1,13 +1,14 @@
 function [t, y] = rk4step(rhs, t_in, y_in, opt)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-arguments (Input)
-   
-end
-
-arguments (Output)
+    %This function uses the rk4step method to approximate the differential
+    %equation. The parameter "rhs" is a function call which evaluates the
+    %differential equation.
     
-end
-
+    h = opt.timestep; % Time step size
+    k1 = h * rhs(t_in, y_in);
+    k2 = h * rhs(t_in + h/2, y_in + k1/2);
+    k3 = h * rhs(t_in + h/2, y_in + k2/2);
+    k4 = h * rhs(t_in + h, y_in + k3);
+    t = t_in + h; 
+    y = y_in + (k1 + 2*k2 + 2*k3 + k4) / 6;
 
 end
